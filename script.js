e44afa1c632e92b9a373a9203fa1c2034eb7177f
yevheniia1106 <165066117+yevheniia1106@users.noreply.github.com>
2024-05-13 11:01:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // JavaScript
 
-document.getElementById("check4").addEventListener("click", f1)
-document.getElementById("reset4").addEventListener("click", f2)
+document.getElementById("check").addEventListener("click", f1)
+document.getElementById("reset").addEventListener("click", f2)
 
 function f1(){
 
-  let number1 = document.getElementById("a4")
-  let number2 = document.getElementById("b4")
+  let number1 = document.getElementById("a")
+  let number2 = document.getElementById("b")
   let result = document.getElementById("res")
 
   let a = parseInt(number1.value)
@@ -18,39 +18,39 @@ function f1(){
 
   if (oper == "add"){let summa = a + b
       if (res == summa){
-      document.getElementById("message4").style.color = "green"
-      document.getElementById("message4").innerHTML = "Correct :)<br>Congrats!"
+      document.getElementById("message").style.color = "green"
+      document.getElementById("message").innerHTML = "Correct :)<br>Congrats!"
       }
       else {
-      document.getElementById("message4").style.color = "red"
-      document.getElementById("message4").innerHTML = "Wrong :(<br>Try again!"
+      document.getElementById("message").style.color = "red"
+      document.getElementById("message").innerHTML = "Wrong :(<br>Try again!"
       }
   } else if (oper == "sub"){let substr = a - b
       if (res == substr){
-      document.getElementById("message4").style.color = "green"
-      document.getElementById("message4").innerHTML = "Correct :)<br>Congrats!"
+      document.getElementById("message").style.color = "green"
+      document.getElementById("message").innerHTML = "Correct :)<br>Congrats!"
       }
       else {
-      document.getElementById("message4").style.color = "red"
-      document.getElementById("message4").innerHTML = "Wrong :(<br>Try again!"
+      document.getElementById("message").style.color = "red"
+      document.getElementById("message").innerHTML = "Wrong :(<br>Try again!"
       }
   } else {let multip = a * b
       if (res == multip){
-      document.getElementById("message4").style.color = "green"
-      document.getElementById("message4").innerHTML = "Correct :)<br>Congrats!"
+      document.getElementById("message").style.color = "green"
+      document.getElementById("message").innerHTML = "Correct :)<br>Congrats!"
       }
       else {
-      document.getElementById("message4").style.color = "red"
-      document.getElementById("message4").innerHTML = "Wrong :(<br>Try again!"
+      document.getElementById("message").style.color = "red"
+      document.getElementById("message").innerHTML = "Wrong :(<br>Try again!"
       }
     }
 } //end of f41
   
 function f2(){
-  document.getElementById("a4").value = ""
-  document.getElementById("b4").value = ""
+  document.getElementById("a").value = ""
+  document.getElementById("b").value = ""
   document.getElementById("res").value = ""
-  document.getElementById("message4").style.color = "black"
-  document.getElementById("message4").innerHTML = "Сheck yourself!<br> When you are ready push on button!"
+  document.getElementById("message").style.color = "black"
+  document.getElementById("message").innerHTML = "Сheck yourself!<br> When you are ready push on button!"
 }  
     
