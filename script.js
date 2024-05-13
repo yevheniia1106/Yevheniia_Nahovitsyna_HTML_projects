4f778f1874b4a1eae351a76116c9591d01bb2907
yevheniia1106 <165066117+yevheniia1106@users.noreply.github.com>
2024-05-14 00:36:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,61 @@
 // JavaScript
 
+// console.log ("hellow")
+
+document.getElementById("estim").addEventListener("input", checkall)
 document.getElementById("check").addEventListener("click", f1)
 document.getElementById("reset").addEventListener("click", f2)
 
-function f1(){
-
   let number1 = document.getElementById("a")
   let number2 = document.getElementById("b")
-  let result = document.getElementById("res")
+  let estimated = document.getElementById("estim")
+  let select = document.getElementById("oper") // выбираем элемент select
 
+
+function checkall(){
   let a = parseInt(number1.value)
   let b = parseInt(number2.value)
-  let res = parseInt(result.value)
+  let estim = parseInt(estimated.value)
+  let oper = select.value // получаем значение выбранного элемента
+  if (Number.isInteger(a) && Number.isInteger(b)
+  && Number.isInteger(estim) && oper != "define")
+  {document.getElementById("check").removeAttribute("disabled")
+  }
+}
 
-  let select = document.getElementById("oper"); // выбираем элемент select
-  let oper = select.value; // получаем значение выбранного элемента
+function f1(){
+  let a = parseInt(number1.value)
+  let b = parseInt(number2.value)
+  let estim = parseInt(estimated.value)
+  let oper = select.value // получаем значение выбранного элемента
 
-  if (oper == "add"){let summa = a + b
-      if (res == summa){
-      document.getElementById("message").style.color = "green"
-      document.getElementById("message").innerHTML = "Correct :)<br>Congrats!"
-      }
-      else {
-      document.getElementById("message").style.color = "red"
-      document.getElementById("message").innerHTML = "Wrong :(<br>Try again!"
-      }
-  } else if (oper == "sub"){let substr = a - b
-      if (res == substr){
-      document.getElementById("message").style.color = "green"
-      document.getElementById("message").innerHTML = "Correct :)<br>Congrats!"
-      }
-      else {
-      document.getElementById("message").style.color = "red"
-      document.getElementById("message").innerHTML = "Wrong :(<br>Try again!"
-      }
-  } else {let multip = a * b
-      if (res == multip){
-      document.getElementById("message").style.color = "green"
-      document.getElementById("message").innerHTML = "Correct :)<br>Congrats!"
-      }
-      else {
-      document.getElementById("message").style.color = "red"
-      document.getElementById("message").innerHTML = "Wrong :(<br>Try again!"
-      }
+  switch (oper) {
+    case "add":
+      res = a + b
+    break;
+    case "sub":
+      res = a - b
+    break;
+    case "mult":
+      res = a * b
+    break;
+  }
+
+  if (estim == res){
+    document.getElementById("message").style.color = "green"
+    document.getElementById("message").innerHTML = "Correct :)<br>Congrats!"
+    }
+    else {
+    document.getElementById("message").style.color = "red"
+    document.getElementById("message").innerHTML = "Wrong :(<br>Try again!"
     }
 } //end of f1
   
 function f2(){
   document.getElementById("a").value = ""
   document.getElementById("b").value = ""
-  document.getElementById("res").value = ""
+  document.getElementById("estim").value = ""
+  document.getElementById("oper").value = "define"
   document.getElementById("message").style.color = "black"
   document.getElementById("message").innerHTML = "Сheck yourself!<br> When you are ready push on button!"
 }  
